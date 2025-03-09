bedda8b2bb54e1750b4d760506387108208c9121
sebasttis <jhon123seba@gmail.com>
2025-03-09 14:26:54 -0500

    cambios

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -10,7 +10,7 @@ class Cors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', '*') // Permite peticiones de cualquier origen
+            ->header('Access-Control-Allow-Origin', 'https://front1-production.up.railway.app') // Permite peticiones de cualquier origen
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
             ->header('Access-Control-Allow-Credentials', 'true'); // Esto es clave
