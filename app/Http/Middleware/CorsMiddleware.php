f7dfb221f2b099c22742beb7e0a150962016dc59
sebasttis <jhon123seba@gmail.com>
2025-03-09 14:38:03 -0500

    ola

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -14,6 +14,7 @@ class CorsMiddleware
         $response->headers->set('Access-Control-Allow-Origin', 'https://front1-production.up.railway.app');
         $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
         
 
         return $response;
