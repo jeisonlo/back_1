64b80202e607684d0e585f9b0075f55faff29e30
jeison <youceronjeison46@gmail.com>
2025-04-23 10:38:18 -0500

    ahd

--- app/Http/Controllers/ExerciseLikeController.php
+++ app/Http/Controllers/ExerciseLikeController.php
@@ -2,14 +2,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\ExerciseLike;
+use App\Models\exerciseLike;
 
 class ExerciseLikeController extends Controller {
     // Dar like a un ejercicio
     public function like($exerciseId) {
         // Evitar duplicados
-        if (!ExerciseLike::where('exercise_id', $exerciseId)->exists()) {
-            ExerciseLike::create([
+        if (!exerciseLike::where('exercise_id', $exerciseId)->exists()) {
+            exerciseLike::create([
                 'exercise_id' => $exerciseId
             ]);
         }
@@ -18,13 +18,13 @@ class ExerciseLikeController extends Controller {
 
     // Quitar like a un ejercicio
     public function unlike($exerciseId) {
-        ExerciseLike::where('exercise_id', $exerciseId)->delete();
+        exerciseLike::where('exercise_id', $exerciseId)->delete();
         return response()->json(['message' => 'Like eliminado'], 200);
     }
 
     // Obtener ejercicios con like
     public function getLikedExercises() {
-        $likedExercises = ExerciseLike::pluck('exercise_id');
+        $likedExercises = exerciseLike::pluck('exercise_id');
         return response()->json($likedExercises);
     }
 }
